1a6e64b22a94b0eb92fa180d0c57e0fd68fd1c66
Kevin Arce <iav.kevinarce@ufg.edu.sv>
2022-06-17 21:22:23 -0600

    Added Mean squared error

--- robust.py
+++ robust.py
@@ -12,7 +12,7 @@ if __name__ == "__main__": # if this file is run directly, run the following cod
     dataset = pd.read_csv("data/felicidad_corrupt.csv") # load data 
     print(dataset.head(5)) # print first 5 rows of dataset 
 
-    X = dataset(['country', 'score'], axis = 1) # create X matrix 
+    X = dataset.drop(['country', 'score'], axis = 1) # create X matrix 
     y = dataset['score'] # create y matrix
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) # split data into training and testing sets with 30% test size and random state 42
@@ -21,4 +21,12 @@ if __name__ == "__main__": # if this file is run directly, run the following cod
         'SVR': SVR(gamma= 'auto', C=1, epsilon=0.1), # SVR is a support vector regression model. Gamma is the kernel coefficient for the RBF kernel. C is the penalty parameter for the error term. Epsilon is the epsilon parameter for the epsilon-insensitive loss function.
         'RANSAC': RANSACRegressor(), # RANSACRegressor is a robust estimator that is resistant to outliers.
         'Huber': HuberRegressor(epsilon=1.35) # HuberRegressor is a robust estimator that is resistant to outliers. epsilon is the epsilon parameter for the Huber loss function.
-    }
+    } # end estimators
+
+    for name, estimador in estimadors.items(): # for each estimator in estimadors dictionary 
+        estimador.fit(X_train, y_train) # fit estimator to training data 
+        y_predict = estimador.predict(X_test) # predict y values using estimator 
+
+        print(80*"=") # print divider
+        print(name) # print estimator name
+        print("MSE:", mean_squared_error(y_test, y_predict)) # print mean squared error of estimator 
